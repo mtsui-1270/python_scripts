1069692106c02e7aa3413589620b0d52b3025111
mtsui-1270 <113266980+mtsui-1270@users.noreply.github.com>
2022-09-10 17:33:52 -0700

    Add files via upload

--- Helloworld.py
+++ Helloworld.py
@@ -1,8 +1,8 @@
 
 print("hello world")
-#this is a comment
-# hello_world = "hello world!"
-# print(hello_world)
+
+
+
 
 number_50 = 50
 number_100 = 100
@@ -13,26 +13,9 @@ number_25 = "25"
 number_var = number_50 + number_100 + int(number_25)
 # print("number_175:" , number_var)
 num_175 = "number_175:"
-# print(f"{num_175} {number_var}")
-#num_175 = f"number_{number_var}:"
-#print(num_175)
-#print(num_175.title())
-#print("\tpython")
-#print("\npython")
-#print("Mariah\nMa\nTsui")
-#name_color = " Mariahblue "
-#name_color.rstrip()
-#print(name_color)
-#name_color.lstrip()
-#name_color = "Mariah\nblue"
-#print(name_color)
-#fruits = ['apple' , 'kiwi' , 'peach' , 'pineapple']
-#print(fruits[3])
-#print(fruits[-2])
-#message= f"My favorite fruit is {fruits[0]}."
-#print(message)
-#Chapter 3 Problem Sets
-#names=['Mariah', ' Zoe' , ' Gwen ',' Karina ', ' Alexa ', ' Angelina ']
+
+
+
 print(names[0])
 print(names[1])
 print(names[2])
